0c6b4b51a7585cb3330da6ccb77ed64b5cdaa3e9
Shawn Zhong <public@shawnzhong.com>
2019-05-30 00:54:23 -0500

    bug fix for hex/dec not displayed correctly

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,9 @@
 class Memory {
     static changeRadix(radixStr) {
-        const radix = Number.parseInt(radixStr);
+        this.radix = Number.parseInt(radixStr);
         for (const line of this.lines) {
             for (const word of line.wordList) {
-                word.radix = radix;
+                word.radix = this.radix;
                 word.valueElement.innerText = word.getValueInnerText()
             }
         }
@@ -15,13 +15,15 @@ class DataSegment extends Memory {
         Elements.userData.innerHTML = '';
         Elements.kernelData.innerHTML = '';
 
+        this.radix = 16;
+
         // user data
         this.userData = Module.getUserData();
         this.userLines = [];
         for (let i = 0; i < DataSegment.userData.length / 16; i++) {
             const addr = 0x10000000 + i * 0x10;
-            if (this.isLineEmpty(addr, this.getUserContent)) continue;
-            const newLine = new MemoryLine(addr, this.getUserContent);
+            if (this.isUserLineEmpty(addr)) continue;
+            const newLine = new UserDataMemoryLine(addr);
             newLine.updateValues();
             Elements.userData.append(newLine.element);
             this.userLines.push(newLine);
@@ -33,8 +35,8 @@ class DataSegment extends Memory {
         this.kernelLines = [];
         for (let i = 0; i < DataSegment.kernelData.length / 16; i++) {
             const addr = 0x90000000 + i * 0x10;
-            if (this.isLineEmpty(addr, this.getKernelContent)) continue;
-            const newLine = new MemoryLine(addr, this.getKernelContent);
+            if (this.isKernelLineEmpty(addr)) continue;
+            const newLine = new KernelDataMemoryLine(addr);
             newLine.updateValues();
             Elements.kernelData.append(newLine.element);
             this.kernelLines.push(newLine);
@@ -48,18 +50,24 @@ class DataSegment extends Memory {
         this.userLines.forEach(e => e.updateValues());
     }
 
-    static getUserContent(addr) {
+    static isKernelLineEmpty(addr) {
+        for (let i = addr; i < addr + 0x10; i += 4)
+            if (this.getKernelDataContent(i) !== 0) return false;
+        return true;
+    }
+
+    static isUserLineEmpty(addr) {
+        for (let i = addr; i < addr + 0x10; i += 4)
+            if (this.getUserDataContent(i) !== 0) return false;
+        return true;
+    }
+
+    static getUserDataContent(addr) {
         return DataSegment.userData[(addr - 0x10000000) >> 2];
     }
 
-    static getKernelContent(addr) {
+    static getKernelDataContent(addr) {
         return DataSegment.kernelData[(addr - 0x90000000) >> 2];
-    }
-
-    static isLineEmpty(addr, getContent) {
-        for (let i = addr; i < addr + 0x10; i += 4)
-            if (getContent(i) !== 0) return false;
-        return true;
     }
 
     static toggleKernelData(shoeKernelData) {
@@ -75,6 +83,7 @@ class DataSegment extends Memory {
 class Stack extends Memory {
     static init() {
         Elements.stack.innerHTML = '';
+        this.radix = 16;
         this.stack = Module.getStack();
         this.lines = [];
         this.addNewLines(0x80000000);
@@ -83,7 +92,7 @@ class Stack extends Memory {
 
     static addNewLines(endAddr) {
         for (; endAddr >= RegisterUtils.getSP(); endAddr -= 0x10) {
-            const newLine = new MemoryLine(endAddr - 0x10, this.getContent);
+            const newLine = new StackMemoryLine(endAddr - 0x10);
             Elements.stack.prepend(newLine.element);
             this.lines.push(newLine);
         }
@@ -96,7 +105,7 @@ class Stack extends Memory {
         this.lines.forEach(e => e.updateValues());
     }
 
-    static getContent(addr) {
+    static getStackContent(addr) {
         if (RegisterUtils.getSP() > addr) return undefined;
         const index = Stack.stack.length - (0x80000000 - addr) / 4;
         return Stack.stack[index];
@@ -104,11 +113,11 @@ class Stack extends Memory {
 }
 
 
-class MemoryLine {
-    constructor(startAddress, getContent) {
+class AbstractMemoryLine {
+    constructor(startAddress) {
         this.wordList = [];
         for (let address = startAddress; address < startAddress + 0x10; address += 4)
-            this.wordList.push(new MemoryWord(address, getContent));
+            this.wordList.push(this.createMemoryWord(address));
 
         this.element = document.createElement('span');
         this.element.innerHTML = `[<span class='hljs-attr'>${startAddress.toString(16)}</span>] `;
@@ -124,13 +133,34 @@ class MemoryLine {
     updateValues() {
         this.wordList.forEach(e => e.updateValue());
     }
+
+    createMemoryWord(address) {
+        return new AbstractMemoryWord(address);
+    }
 }
 
-class MemoryWord {
-    constructor(address, getContent) {
+class UserDataMemoryLine extends AbstractMemoryLine {
+    createMemoryWord(address) {
+        return new UserDataMemoryWord(address);
+    }
+}
+
+class KernelDataMemoryLine extends AbstractMemoryLine {
+    createMemoryWord(address) {
+        return new KernelDataMemoryWord(address);
+    }
+}
+
+class StackMemoryLine extends AbstractMemoryLine {
+    createMemoryWord(address) {
+        return new StackMemoryWord(address);
+    }
+}
+
+class AbstractMemoryWord {
+    constructor(address) {
         this.address = address;
-        this.getContent = getContent;
-        this.radix = 16;
+        this.radix = this.getRadix();
 
         this.valueElement = document.createElement('span');
         this.valueElement.classList.add('data-number');
@@ -189,5 +219,43 @@ class MemoryWord {
         return asciiArray
             .map(e => e >= 32 && e < 127 ? e : 183)
             .map(e => String.fromCharCode(e)).join('');
+    }
+
+    getContent(addr) {
+        return 0;
+    }
+
+    getRadix() {
+        return 16;
+    }
+}
+
+class UserDataMemoryWord extends AbstractMemoryWord {
+    getContent(addr) {
+        return DataSegment.getUserDataContent(addr);
+    }
+
+    getRadix() {
+        return DataSegment.radix;
+    }
+}
+
+class KernelDataMemoryWord extends AbstractMemoryWord {
+    getContent(addr) {
+        return DataSegment.getKernelDataContent(addr);
+    }
+
+    getRadix() {
+        return DataSegment.radix;
+    }
+}
+
+class StackMemoryWord extends AbstractMemoryWord {
+    getContent(addr) {
+        return Stack.getStackContent(addr);
+    }
+
+    getRadix() {
+        return Stack.radix;
     }
 }
